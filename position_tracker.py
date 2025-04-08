63a1c8464c1a9cc1cb0ba097423732074ffaa18e
MRX8024 <57844100+MRX8024@users.noreply.github.com>
2025-04-09 00:26:17 +0300

    position_tracker: update

--- position_tracker.py
+++ position_tracker.py
@@ -9,111 +9,35 @@ import chelper
 from . import force_move
 
 class Move:
-    def __init__(self, toolhead, start_pos, end_pos, speed):
-        self.toolhead = toolhead
+    def __init__(self, toolhead, start_pos, end_pos, speed, accel):
         self.start_pos = tuple(start_pos)
         self.end_pos = tuple(end_pos)
-        self.accel = toolhead.max_accel
-        self.junction_deviation = toolhead.junction_deviation
-        self.timing_callbacks = []
+        self.accel = accel = toolhead.max_accel
         velocity = min(speed, toolhead.max_velocity)
-        self.is_kinematic_move = True
         self.axes_d = axes_d = [end_pos[i] - start_pos[i] for i in (0, 1, 2, 3)]
         self.move_d = move_d = math.sqrt(sum([d*d for d in axes_d[:3]]))
-        if move_d < .000000001:
-            # Extrude only move
-            self.end_pos = (start_pos[0], start_pos[1], start_pos[2],
-                            end_pos[3])
-            axes_d[0] = axes_d[1] = axes_d[2] = 0.
-            self.move_d = move_d = abs(axes_d[3])
-            inv_move_d = 0.
-            if move_d:
-                inv_move_d = 1. / move_d
-            self.accel = 99999999.9
-            velocity = speed
-            self.is_kinematic_move = False
-        else:
-            inv_move_d = 1. / move_d
+        inv_move_d = 1. / move_d
         self.axes_r = [d * inv_move_d for d in axes_d]
-        self.min_move_t = move_d / velocity
-        # Junction speeds are tracked in velocity squared.  The
-        # delta_v2 is the maximum amount of this squared-velocity that
-        # can change in this move.
-        self.max_start_v2 = 0.
-        self.max_cruise_v2 = velocity**2
-        self.delta_v2 = 2.0 * move_d * self.accel
-        self.max_smoothed_v2 = 0.
-        self.smooth_delta_v2 = 2.0 * move_d * toolhead.max_accel_to_decel
-        self.next_junction_v2 = 999999999.9
-    def limit_speed(self, speed, accel):
-        speed2 = speed**2
-        if speed2 < self.max_cruise_v2:
-            self.max_cruise_v2 = speed2
-            self.min_move_t = self.move_d / speed
-        self.accel = min(self.accel, accel)
-        self.delta_v2 = 2.0 * self.move_d * self.accel
-        self.smooth_delta_v2 = min(self.smooth_delta_v2, self.delta_v2)
-    def limit_next_junction_speed(self, speed):
-        self.next_junction_v2 = min(self.next_junction_v2, speed**2)
-    def move_error(self, msg="Move out of range"):
-        ep = self.end_pos
-        m = "%s: %.3f %.3f %.3f [%.3f]" % (msg, ep[0], ep[1], ep[2], ep[3])
-        return self.toolhead.printer.command_error(m)
-    def calc_junction(self, prev_move):
-        if not self.is_kinematic_move or not prev_move.is_kinematic_move:
-            return
-        # Allow extruder to calculate its maximum junction
-        extruder_v2 = self.toolhead.extruder.calc_junction(prev_move, self)
-        # Find max velocity using "approximated centripetal velocity"
-        axes_r = self.axes_r
-        prev_axes_r = prev_move.axes_r
-        junction_cos_theta = -(axes_r[0] * prev_axes_r[0]
-                               + axes_r[1] * prev_axes_r[1]
-                               + axes_r[2] * prev_axes_r[2])
-        if junction_cos_theta > 0.999999:
-            return
-        # Apply limits
-        self.max_start_v2 = min(
-            extruder_v2, self.max_cruise_v2, prev_move.max_cruise_v2,
-            self.next_junction_v2, prev_move.max_start_v2 + prev_move.delta_v2)
-        if junction_cos_theta >= -0.999999:
-            if (math.acos(junction_cos_theta) >
-                    math.radians(self.toolhead.scv_angle_threshold)):
-                sin_theta_d2 = min(max(math.sqrt(0.5 * (1. - round(
-                    junction_cos_theta * self.toolhead.scv_boost_coeff, 5))
-                                                 ), 0.000001), 0.999999)
-            else:
-                sin_theta_d2 = math.sqrt(0.5 * (1.0 - junction_cos_theta))
-            R_jd = sin_theta_d2 / (1. - sin_theta_d2)
-            # Approximated circle must contact moves no further than mid-move
-            tan_theta_d2 = sin_theta_d2 / math.sqrt(.5*(1.0+junction_cos_theta))
-            move_centripetal_v2 = .5 * self.move_d * tan_theta_d2 * self.accel
-            prev_move_centripetal_v2 = (.5 * prev_move.move_d * tan_theta_d2
-                                        * prev_move.accel)
-            self.max_start_v2 = min(
-                self.max_start_v2,
-                R_jd * self.junction_deviation * self.accel,
-                R_jd * prev_move.junction_deviation * prev_move.accel,
-                move_centripetal_v2, prev_move_centripetal_v2)
-        self.max_smoothed_v2 = min(self.max_start_v2,
-            prev_move.max_smoothed_v2 + prev_move.smooth_delta_v2)
-    def set_junction(self, start_v2, cruise_v2, end_v2):
-        # Determine accel, cruise, and decel portions of the move distance
-        half_inv_accel = .5 / self.accel
-        accel_d = (cruise_v2 - start_v2) * half_inv_accel
-        decel_d = (cruise_v2 - end_v2) * half_inv_accel
-        cruise_d = self.move_d - accel_d - decel_d
-        # Determine move velocities
-        self.start_v = start_v = math.sqrt(start_v2)
-        self.cruise_v = cruise_v = math.sqrt(cruise_v2)
-        self.end_v = end_v = math.sqrt(end_v2)
-        # Determine time spent in each portion of move (time is the
-        # distance divided by average velocity)
-        self.accel_t = accel_d / ((start_v + cruise_v) * 0.5)
-        self.cruise_t = cruise_d / cruise_v
-        self.decel_t = decel_d / ((end_v + cruise_v) * 0.5)
-        full_t = self.accel_t + self.cruise_t + self.decel_t
-        self.avgspeed = self.move_d / full_t
+        v_cruise = velocity
+        accel_d = v_cruise ** 2 / (2 * accel)
+        accel_decel_d = accel_d + accel_d
+        if accel_decel_d >= move_d:
+            v_peak = math.sqrt(move_d * accel)
+            self.start_v = 0.
+            self.cruise_v = v_peak
+            self.end_v = 0.
+            self.accel_t = v_peak / accel
+            self.cruise_t = 0.
+            self.decel_t = v_peak / accel
+        else:
+            self.start_v = 0.
+            self.cruise_v = v_cruise
+            self.end_v = 0.
+            self.accel_t = v_cruise / accel
+            cruise_d = move_d - accel_decel_d
+            self.cruise_t = cruise_d / v_cruise
+            self.decel_t = v_cruise / accel
+
 
 class CorexyMoveChelper:
     def __init__(self, printer):
@@ -126,16 +50,16 @@ class CorexyMoveChelper:
         self.trapq = ffi_main.gc(ffi_lib.trapq_alloc(), ffi_lib.trapq_free)
         self.trapq_append = ffi_lib.trapq_append
         self.trapq_finalize_moves = ffi_lib.trapq_finalize_moves
-        self.corexy_kins = None
+        self.corexy_kins = {'extra': {}, 'orig': {}}
         self._setup_itersolve(ffi_main, ffi_lib)
+        self.queue = []
 
     def _setup_itersolve(self, ffi_main, ffi_lib):
-        self.corexy_kins = kins = {'extra': {}, 'orig': {}}
         p = [b'+', b'-']
         for i, rail in enumerate(self.rails):
             for st in rail.get_steppers():
                 name = st.get_name()
-                kins['extra'][name] = ffi_main.gc(
+                self.corexy_kins['extra'][name] = ffi_main.gc(
                     ffi_lib.corexy_stepper_alloc(p[i]), ffi_lib.free)
 
     def dump_kinematics(self):
@@ -176,15 +100,13 @@ class CorexyMoveChelper:
         for rail in self.rails:
             rail.generate_steps(flush_time)
 
-    def debug_set_kin(self):
-        self.dump_kinematics()
-        # self.set_kinematics('extra')
+    def debug_set_kin(self, dists):
+        # self.set_extra_kinematics()
         # self.set_trapq(self.trapq)
         # self.set_position((0., 0., 0.))
         # self.set_trapq(self.toolhead.get_trapq())
-        # self.set_kinematics('orig')
-        self._manual_move(25)
-        self._manual_move(-25)
+        # self.set_orig_kinematics()
+        self.multi_move(dists)
 
     def _manual_move(self, dist, speed=10., accel=5000.):
         toolhead = self.toolhead
@@ -215,47 +137,51 @@ class CorexyMoveChelper:
         toolhead.dwell(accel_t + cruise_t + accel_t)
         toolhead.flush_step_generation()
 
-    def multi_move(self, dists, speed=50., accel=5000.):
+    def multi_move(self, dists, speed=500., accel=20000.):
         toolhead = self.toolhead
+        move = Move(toolhead, (0, 0, 0, 0), (*dists, 0, 0), speed, accel)
         toolhead.flush_step_generation()
         self.set_extra_kinematics()
         self.set_trapq(self.trapq)
         self.set_position((0., 0., 0.))
-        axis_r, accel_t, cruise_t, cruise_v = force_move.calc_move_time(dists[0], speed, accel)
-        axis_r1, *_ = force_move.calc_move_time(dists[1], speed, accel)
+        # axis_r, accel_t, cruise_t, cruise_v = force_move.calc_move_time(dists[0], speed, accel)
+        # axis_r1, *_ = force_move.calc_move_time(dists[1], speed, accel)
         # axes_d = [dists[i] - 0 for i in (0, 1)]
         # inv_move_d = 1. / math.sqrt(sum([d * d for d in axes_d]))
         # axes_r = [d * inv_move_d for d in axes_d]
-        print_time = toolhead.get_last_move_time()
+
         ##############################################################################################
         # now = self.printer.get_reactor().monotonic()
         # etime = toolhead.mcu.estimated_print_time(now)
-        # print_time = etime + 0.2
+        # print_time = etime + 0.5
         # self.gcode.respond_info(f"multi_move: axes_r={axes_r} delta_t={print_time-etime:.3f} print_time={print_time:.3f} etime={etime:.3f}")
         ##############################################################################################
-        self.trapq_append(self.trapq, print_time, accel_t, cruise_t, accel_t,
-                          0., 0., 0., axis_r, axis_r1, 0., 0., cruise_v, accel)
-        # move = Move(toolhead, (0, 0, 0, 0), (*dists, 0, 0), speed)
-        # move.set_junction(0, speed**2, 0)
-        # self.gcode.respond_info(f"multi_move: start_pos={move.start_pos} "
-        #                         f"end_pos={move.end_pos} accel_t={move.accel_t} "
-        #                         f"cruise_t={move.cruise_t} decel_t={move.decel_t}")
-        # self.trapq_append(
-        #     self.trapq, print_time, move.accel_t, move.cruise_t,
-        #     move.decel_t, move.start_pos[0], move.start_pos[1],
-        #     move.start_pos[2], move.axes_r[0], move.axes_r[1],
-        #     move.axes_r[2], move.start_v, move.cruise_v, move.accel)
-        # move_time = move.accel_t + move.cruise_t + move.decel_t
-        move_time = accel_t + cruise_t + accel_t
+        # self.trapq_append(self.trapq, print_time, accel_t, cruise_t, accel_t,
+        #                   0., 0., 0., axis_r, axis_r1, 0., 0., cruise_v, accel)
+
+        # move.set_junction(move.max_start_v2, move.max_cruise_v2, 0)
+        print_time = toolhead.get_last_move_time()
+        self.gcode.respond_info(f"multi_move: start_pos={move.start_pos} "
+                                f"end_pos={move.end_pos} accel_t={move.accel_t} "
+                                f"cruise_t={move.cruise_t} decel_t={move.decel_t}")
+        self.trapq_append(
+            self.trapq, print_time, move.accel_t, move.cruise_t,
+            move.decel_t, move.start_pos[0], move.start_pos[1],
+            move.start_pos[2], move.axes_r[0], move.axes_r[1],
+            move.axes_r[2], move.start_v, move.cruise_v, move.accel)
+        move_time = move.accel_t + move.cruise_t + move.decel_t
+        # move_time = accel_t + cruise_t + accel_t
         print_time = print_time + move_time
         self.generate_steps(print_time)
         self.trapq_finalize_moves(self.trapq, print_time + 99999.9,
                                   print_time + 99999.9)
         self.set_trapq(toolhead.get_trapq())
         self.set_orig_kinematics()
+        # toolhead.wait_moves()
         toolhead.note_mcu_movequeue_activity(print_time)
         toolhead.dwell(move_time)
         toolhead.flush_step_generation()
+        return print_time
 
     # def _orig_manual_move(self, dist, speed=10., accel=5000.):
     #     toolhead = self.toolhead
@@ -299,19 +225,22 @@ class PositionRestorer:
         dir = 1 if dist >= 0 else -1
         rd, _ = tracker.mcu_stepper.get_rotation_distance()
         single_d = rd / tracker.stepper_fspr * 4
-        return math.ceil(abs(dist / single_d)) * single_d * dir
+        return math.floor(abs(dist / single_d)) * single_d * dir
 
-    def single_move(self, tracker):
+    def single_move(self, tracker, speed=500., accel=20000.):
+        # toolhead = self.printer.lookup_object('toolhead')
         # mcu_poss = tracker.mcu_stepper.get_mcu_position()
         # self.gcode.respond_info(f"start mcu_pos={mcu_poss}")
         dist, *_ = tracker.calc_position_diff()
-        aligned_dist = self.position_filer(tracker, dist)
+        if not (aligned_dist := self.position_filer(tracker, dist)):
+            return
         self.gcode.respond_info(f"{tracker.sensor_name} move "
                                 f"dist={aligned_dist:.3f}")
         self.force_move.manual_move(tracker.mcu_stepper,
-                                    aligned_dist, 100, 5000)
+                                    aligned_dist, speed, accel)
         # mcu_poss = tracker.mcu_stepper.get_mcu_position()
         # self.gcode.respond_info(f"finish mcu_pos={mcu_poss}")
+        # tracker.freeze_time = ptime
         tracker.reset_runout()
         tracker.resume_tracker()
 
@@ -358,22 +287,25 @@ class PositionRestorer:
         diagpos = [tr.calc_position_diff()[0] for tr in trackers]
         relpos = self.corexy_to_cartesian(*diagpos)
         relpos = [self.position_filer(tr, p) for p, tr in zip(relpos, trackers)]
+        if not any(abs(p) > 0 for p in relpos):
+            return
         toolhead = self.printer.lookup_object('toolhead')
         self.gcode.respond_info(f"corexy move rel_x={relpos[0]:.3f} rel_y={relpos[1]:.3f}")
         # mcu_poss = [tr.mcu_stepper.get_mcu_position() for tr in trackers]
         # self.gcode.respond_info(f"start mcu_pos={mcu_poss}")
-        self.corexy_move_chelper.multi_move(relpos)
-        toolhead.wait_moves()
+        ptime = self.corexy_move_chelper.multi_move(relpos)
         # mcu_poss = [tr.mcu_stepper.get_mcu_position() for tr in trackers]
         # self.gcode.respond_info(f"finish mcu_pos={mcu_poss}")
+        # toolhead.wait_moves()
         for i, tr in enumerate(trackers):
+            # tr.freeze_time = ptime
             tr.reset_runout()
             tr.resume_tracker()
 
     def restore_position(self, tracker):
         # tracker.pause_tracker()
         if not all(tr.is_runouted for tr in self.trackers):
-            if self.req_count < 5:
+            if self.req_count < 3:
                 self.req_count += 1
                 return
             self.req_count = 0
@@ -421,6 +353,7 @@ class PositionTracker:
         self.sensor_offset = 0.
         self.mcu_offset = 0.
         self.last_batch = []
+        self.freeze_time = -1
         self.stepper = None
         self.axis = None
         self.is_running = False
@@ -481,7 +414,7 @@ class PositionTracker:
         mcu_past_pos = self.mcu_stepper.get_past_mcu_position(sens_ptime) # + self.mcu_offset
         req_pos = self.mcu_stepper.mcu_to_commanded_position(mcu_past_pos) - self.mcu_offset
         self.resp_clock += 1
-        if self.resp_clock == 10:
+        if self.resp_clock == 2000:
             self.resp_clock = 0
             mcu_pos = self.mcu_stepper.get_mcu_position()
             mcu_pos_offset = self.mcu_stepper._mcu_position_offset
@@ -504,6 +437,9 @@ class PositionTracker:
             return False
         if self.is_paused:
             return True
+        if self.last_batch[0] < self.freeze_time:
+            return True
+        self.freeze_time = -1
         diff_d, req_pos, sens_pos, sens_ptime = self.calc_position_diff()
         if abs(diff_d) < self.max_difference:
             return True
@@ -514,8 +450,7 @@ class PositionTracker:
         #        f'required_pos={req_pos:.3f} sensor_pos={enc_pos:.3f} '
         #        f'etime={est_print_time:.3f} sensor_time={enc_ptime:.3f}')
         # return True
-        msg = (f'{self.sensor_name}: '
-               f'delta_pos={diff_d:.3f} '
+        msg = (f'Skipped steps on {self.sensor_name}: '
                f'req_pos={req_pos:.3f} '
                f'sens_pos={sens_pos:.3f} '
                f'event_ptime={est_print_time:.3f} '
@@ -600,7 +535,9 @@ class PositionTracker:
             self.pause_tracker()
         elif enable == 1:
             self.resume_tracker()
-        # self.restore_chelper.corexy_move_chelper.debug_set_kin()
+        # dists = gcmd.get('DIST', []).split(',')
+        # dists = [float(dist) for dist in dists]
+        # self.restore_chelper.corexy_move_chelper.debug_set_kin(dists)
         self.gcode.respond_info(f"Position tracker {self.sensor_name}: "
                                 f"{self.get_status(None)['status']}")
 
